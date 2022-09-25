c2bb798c7e9b2a7dc3ee5c364cf19d8bba99c26f
ashmanix <theashmanix@gmail.com>
2022-09-25 11:14:53 +0100

    Testing if I can add a menu item that will trigger a call bacl

--- src/plugin-macros.h.in
+++ src/plugin-macros.h.in
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+@CMAKE_PROJECT_NAME@
+Copyright (C) 2022 @PLUGIN_AUTHOR@ @LINUX_MAINTAINER_EMAIL@
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- src/plugin-main.c
+++ src/plugin-main.c
@@ -17,8 +17,17 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
 #include <obs-module.h>
+#include <obs-frontend-api.h>
 
 #include "plugin-macros.generated.h"
+
+void callback_function() {
+	blog(LOG_INFO, "Callback function called!");
+}
+
+void load_menu_option() {
+	 obs_frontend_add_tools_menu_item("Add Countdown Timer", callback_function, NULL);
+}
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
@@ -27,6 +36,7 @@ bool obs_module_load(void)
 {
 	blog(LOG_INFO, "plugin loaded successfully (version %s)",
 	     PLUGIN_VERSION);
+	load_menu_option();
 	return true;
 }
 
